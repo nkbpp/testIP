3bc3af6c515193d4794941cb4b5bea736d64ae4a
Alexandr <alexbratchin@gmail.com>
2017-01-11 11:46:54 +0300

    4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Введите начальный IP: ");
-//        String s1 = in.nextLine();
-//        System.out.println("Введите конечный IP: ");
-//        String s2 = in.nextLine();
-//        short ip1[] = new short[4];
-//        short ip2[] = new short[4];
-//        String s[] = s1.split("\\.");
-//        for (int i = 0; i < s.length; i++) {
-//            ip1[i] = Short.parseShort(s[i]);
-//        }
-//        s = s2.split("\\.");
-//        for (int i = 0; i < s.length; i++) {
-//            ip2[i] = Short.parseShort(s[i]);
-//        }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите начальный IP: ");
+        String s1 = in.nextLine();
+        System.out.println("Введите конечный IP: ");
+        String s2 = in.nextLine();
+        short ip1[] = new short[4];
+        short ip2[] = new short[4];
+        String s[] = s1.split("\\.");
+        for (int i = 0; i < s.length; i++) {
+            ip1[i] = Short.parseShort(s[i]);
+        }
+        String s3[] = s2.split("\\.");
+        for (int i = 0; i < s3.length; i++) {
+            ip2[i] = Short.parseShort(s3[i]);
+        }
 
-        short ip1[] = {255,255,255,1};
-        short ip2[] = {0,0,0,5};
+        //short ip1[] = {255,255,255,1};
+        //short ip2[] = {0,0,0,5};
 
         do{
+            System.out.println(ip1[0]+"."+ip1[1]+"."+ip1[2]+"."+ip1[3]);
             ip1[3]++;
             if(ip1[3]>255){
                 ip1[3]=0;
@@ -37,12 +38,6 @@ public class Main {
                     }
                 }
             }
-            System.out.println(ip1[0]+"."+ip1[1]+"."+ip1[2]+"."+ip1[3]+".");
         }while (!Arrays.equals(ip1, ip2));
-
-//        for (int i:
-//             ip2) {
-//            System.out.println(String.valueOf(i));
-//        }
     }
 }
