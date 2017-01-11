bef4c2446fa8bd162710658032ae22c6f656e160
Alexandr <alexbratchin@gmail.com>
2017-01-11 11:01:53 +0300

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
-        System.out.println("");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите начальный IP: ");
+        String s1 = in.nextLine();
+        System.out.println("Введите конечный IP: ");
+        String s2 = in.nextLine();
+        int ip1[] = new int[4];
+        int ip2[] = new int[4];
+        String s[] = s1.split("\\.");
+        for (int i = 0; i < s.length; i++) {
+            ip1[i] = Integer.parseInt(s[i]);
+        }
+        s = s2.split("\\.");
+        for (int i = 0; i < s.length; i++) {
+            ip2[i] = Integer.parseInt(s[i]);
+        }
+//        for (int i:
+//             ip2) {
+//            System.out.println(String.valueOf(i));
+//        }
     }
 }
