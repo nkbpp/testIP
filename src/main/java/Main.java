478fea9b5dbad6c2725d7ed51dbe915dc1732f30
Alexandr <alexbratchin@gmail.com>
2017-01-11 13:03:22 +0300

    5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,32 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private static final String IPADDRESS =
+                    "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите начальный IP: ");
-        String s1 = in.nextLine();
+        String s1;
+        boolean b = true;
+        do {
+            System.out.println("Введите начальный IP: ");
+            s1 = in.nextLine();
+
+            Pattern p = Pattern.compile(IPADDRESS);
+            Matcher m = p.matcher(s1);
+            if(m.matches()) b = false;
+            else {
+                System.out.println("Неверный формат IP адреса, повторите ввод!!!");
+            }
+        }while (b);
+
         System.out.println("Введите конечный IP: ");
         String s2 = in.nextLine();
         short ip1[] = new short[4];
